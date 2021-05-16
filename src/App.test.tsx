6071780145f05156a2fd530856da1f36ec9ddc63
Yoni <firroloni.yoni@gmail.com>
2021-05-16 21:08:14 +0200

    add test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,10 +4,7 @@ import { LoginPage } from './pages/Login/LoginPage';
 afterEach(cleanup);
 
 test('LoginPage proposes to login before proposing to register.', () => {
-  const {queryByText} = render(
-    <LoginPage />
-  );
+  const { queryByText } = render(<LoginPage />);
 
   expect(queryByText(/login/i)).toBeTruthy();
-
-})
+});
