c66a5f076a55d168e84b99eb240a6ae88543a905
Yoni <firroloni.yoni@gmail.com>
2021-05-16 11:29:04 +0200

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.scss";
 import { Link, Route, BrowserRouter } from "react-router-dom"
 import { LoginPage } from "./pages/Login/LoginPage";
+import logo from "./assets/PicSpyLogo.png"
 import { CodeRunning } from "./pages/code_running/CodeRunning";
 
 
@@ -11,22 +12,26 @@ export const App = () => {
     <BrowserRouter >
       <div className="App">
         <main>
-          <nav>
-            <ul>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/about">About</Link></li>
-              <li><Link to="/contact">Contact</Link></li>
-            </ul>
-          </nav>
+        <div className="header">
+          <div className="logo">
+            <img alt="logo" src={logo} />
+          </div>
+          <div className="navMenu">
+            <nav>
+              <ul>
+                <li><Link to="/">Overview</Link></li>
+                <li><Link to="/">Liste des exercices</Link></li>
+              </ul>
+            </nav>
+          </div>
+        </div>
 
-
-
-          <Route path="/" exact component={LoginPage} />
-           <Route path="/code"  component={CodeRunning} />
-      {/* <Route path="/contact"  component={Contact} /> */}
+        <Route path="/" exact component={LoginPage} />
+        <Route path="/code" component={CodeRunning} />
+        {/* <Route path="/contact"  component={Contact} /> */}
 
         </main>
       </div>
-    </BrowserRouter>
+    </BrowserRouter >
   )
 }
--- src/pages/code_running/CodeRunning.tsx
+++ src/pages/code_running/CodeRunning.tsx
@@ -1,65 +1,46 @@
 import React from 'react';
-import logo from "./PicSpyLogo.png"
-import pattern from "./pattern.svg"
-import "./CodeRunning.css"
+import pattern from "../../assets/pattern.svg"
+import "./CodeRunning.scss"
 import ReactDOM from "react-dom";
 
 import Editor from "@monaco-editor/react";
 import loader from "@monaco-editor/loader";
 
 
-function runOnClick() {
-  loader.init().then(monaco => {
-    console.log('Here is the monaco instance', monaco);
-  });
-}
-export class CodeRunning extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className="App" ref={this.props.containerRef}>
-        <div className="header">
-          <div className="logo">
-            <img src={logo} />
-          </div>
-          <div className="navMenu">
-            <nav>
-              <ul>
-                <li><a href="#">Overview</a></li>
-                <li><a href="#">Liste des exercices</a></li>
-              </ul>
-            </nav>
-          </div>
-        </div>
+export const CodeRunning = () => {
 
-        <div className="body">
-          <div className="navExercises">
-            <nav>
-              <ul>
-                <li><a href="#">Exercice 1</a></li>
-                <li><a href="#">Exercice 2</a></li>
-              </ul>
-            </nav>
-          </div>
-          <div className="consigne">
-            <p>Exercice</p>
-            <p>...........</p>
-          </div>
-          <div className="pattern">
-            <img src={pattern} />
-          </div>
-          <div className="monaco-editor">
-            <Editor
-              height="50vh"
-              defaultLanguage="bash"
-            // defaultValue="// some comment"
-            />
-          </div>
-          <button onClick={runOnClick()}>Run</button>
-        </div>
-      </div>
-    );
+  const runOnClick = () => {
+    console.log("running");
   }
+
+  return (
+    <>
+      <div className="body">
+        <div className="navExercises">
+          <nav>
+            <ul>
+              <li><a href="#">Exercice 1</a></li>
+              <li><a href="#">Exercice 2</a></li>
+            </ul>
+          </nav>
+        </div>
+        <div className="consigne">
+          <p>Exercice</p>
+          <p>...........</p>
+        </div>
+        <div className="pattern">
+          <img alt="pattern" src={pattern} />
+        </div>
+        <div className="monaco-editor">
+          <Editor
+            height="50vh"
+            defaultLanguage="bash"
+          // defaultValue="// some comment"
+          />
+        </div>
+        <button onClick={runOnClick}>Run</button>
+      </div>
+    </>
+  );
+
 }
