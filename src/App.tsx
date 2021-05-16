24de39b9f0f7a7f9495457a13b8e205175fe0b1f
Yoni <firroloni.yoni@gmail.com>
2021-05-16 11:10:12 +0200

    move coderunning component

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,15 @@ import './App.css';
 import {CodeRunning} from "./CodeRunning.jsx"
 
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
+export const App = () => {
+
+
     return(
     <div className="codeRunning">
       <CodeRunning containerRef={ref => (this.current = ref)}/>
     </div>
   );
-  }
+  
 }
 
 export default App;
