a6a0e60a5eae5d55220a1ffdcd00951c68f7e2ed
Yoni <firroloni.yoni@gmail.com>
2021-06-07 12:42:55 +0200

    bind code execution and stdout

--- src/pages/ChallengePage.tsx
+++ src/pages/ChallengePage.tsx
@@ -5,6 +5,7 @@ import Editor from '@monaco-editor/react';
 import Axios from '../axios-config';
 
 type Challenge = {
+  _id: string;
   name: string;
   instructions: string;
   imageUrl: string;
@@ -13,7 +14,7 @@ type Challenge = {
 export const ChallengePage = (props: { match: { params: { id: string } } }) => {
   const [challenge, setChallenge] = useState({} as Challenge);
   const [code, setCode] = useState('');
-  // const [stdout, setStdout] = useState('');
+  const [stdout, setStdout] = useState('');
 
   useEffect(() => {
     const fetchChallenge = async (id: string) => {
@@ -27,9 +28,21 @@ export const ChallengePage = (props: { match: { params: { id: string } } }) => {
   }, [props.match.params.id]);
 
   const runOnClick = async () => {
-    const { data } = await Axios.post(`/attempts`, {
-      code,
-    });
+    const { data } = await Axios.post(
+      `/attempts`,
+      {
+        challenge: challenge._id.split('/').pop(),
+        code,
+        language: 'bash',
+      },
+      {
+        headers: {
+          Authorization:
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkhlbG9pc2UiLCJzdWIiOiI2MGE5MTE1MzA3NmQ0OGVjMTljN2MxYjgiLCJpYXQiOjE2MjMwNjExNjJ9.EpDB71Kd2HmjrSr57Oace5r7efolXK-9Ffiv-lmnmoI',
+        },
+      },
+    );
+    setStdout(data.stdout);
     console.log(data);
   };
 
@@ -41,34 +54,33 @@ export const ChallengePage = (props: { match: { params: { id: string } } }) => {
             <div className="font-bold text-xl mb-2">{challenge.name}</div>
             <p>{challenge.instructions}</p>
           </div>
-          <div className="h-auto text-center bg-gray-600 rounded p-5">
-            <div className="rounded-lg max-h-full overflow-hidden flex flex-col justify-center items-center">
-              <div className="max-h-full w-full">
-                <Editor
-                  defaultLanguage="bash"
-                  height="90vh"
-                  // defaultValue="// some comment"
-                  onChange={(value) => {
-                    setCode(value as string);
-                  }}
-                  theme="vs-dark"
-                />
-              </div>
-              <div className="h-auto w-full grid grid-cols-12 justify-center overflow-hidden rounded-b">
-                <div className="bg-black max-h-full h-full col-span-11 p-2 text-left font-light">
-                  Sortie
-                </div>
-                <div className="bg-black col-span-1 flex justify-center items-center">
-                  <button
-                    className="bg-blue-500 px-2 rounded max-h-10"
-                    onClick={runOnClick}
-                  >
-                    Run
-                  </button>
-                </div>
+          {/* <div className="h-auto text-center bg-gray-600 rounded p-5"> */}
+          <div className="rounded-lg h-full overflow-hidden flex flex-col justify-center items-center">
+            <div className="relative max-h-full w-full">
+              <Editor
+                defaultLanguage="bash"
+                height="80vh"
+                // defaultValue="// some comment"
+                onChange={(value) => {
+                  setCode(value as string);
+                }}
+                theme="vs-dark"
+              />
+
+              <button
+                className="absolute top-0 right-0 bg-blue-500 px-2 rounded-bl-lg h-10 w-20 "
+                onClick={runOnClick}
+              >
+                Run
+              </button>
+            </div>
+            <div className="h-full w-full overflow-hidden rounded-b">
+              <div className="bg-black max-h-full h-full p-2 w-full text-left font-light">
+                {stdout || "Aucune sortie après l'execution de ce code."}
               </div>
             </div>
           </div>
+          {/* </div> */}
           <div className="h-auto text-center bg-gray-600 rounded p-5">
             <img
               className="h-full max-h-full object-contain"
