92cff86df72ae616369521c1b81e74af7f16f3e2
Yoni <firroloni.yoni@gmail.com>
2021-06-26 03:45:20 +0200

    implement registering and fix page

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,35 @@ import About from './pages/About';
 import { ChallengePage } from './pages/ChallengePage';
 import Home from './pages/Home';
 import { LoginPage } from './pages/LoginPage';
+import { AuthProvider } from 'react-auth-kit';
+import { Toaster } from 'react-hot-toast';
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Navbar />
-        <main>
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/about" exact component={About} />
-            <Route path="/sign-up" exact component={LoginPage} />
-            <Route path="/challenges/:id" exact component={ChallengePage} />
+    <AuthProvider
+      authType={'cookie'}
+      authName={'_whoami'}
+      cookieDomain={window.location.hostname}
+      cookieSecure={window.location.protocol === 'https:'}
+    >
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <main>
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/about" exact component={About} />
+              <Route path="/sign-up" exact component={LoginPage} />
+              <Route path="/challenges/:id" exact component={ChallengePage} />
 
-            {/* <Route path="/about"  component={About} />
+              {/* <Route path="/about"  component={About} />
       <Route path="/contact"  component={Contact} /> */}
-          </Switch>
-        </main>
-      </div>
-    </BrowserRouter>
+            </Switch>
+          </main>
+        </div>
+      </BrowserRouter>
+      <Toaster />
+    </AuthProvider>
   );
 };
 export default App;
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,10 +1,9 @@
 import { useState } from 'react';
 import { LoginForm } from '../components/LoginForm';
 import { RegisterForm } from '../components/RegisterForm';
-import './Login.scss';
 
 export const LoginPage = () => {
-  const [loggingIn, setLoggingIn] = useState(false);
+  const [loggingIn, setLoggingIn] = useState(true);
 
   const switchState = () => {
     const currentLoggingInState = loggingIn;
@@ -12,14 +11,15 @@ export const LoginPage = () => {
   };
 
   return (
-    <div className="login">
-      <div className="container">
+    <div className="grid justify-items-center bg-gray-800 p-10">
+      <div className="bg-gray-600 rounded h-min px-16 py-6 transition-all relative">
+        <button
+          className="absolute top-0 right-0 bg-blue-500 rounded-bl-lg h-10 w-20"
+          onClick={switchState}
+        >
+          {loggingIn ? 'Register' : 'Login'}
+        </button>
         {loggingIn ? <LoginForm /> : <RegisterForm />}
-      </div>
-      <div className={`switch-button ${loggingIn ? 'right' : 'left'}`}>
-        <div className="inner-container" onClick={switchState}>
-          <div className="text">{loggingIn ? 'Register' : 'Login'}</div>
-        </div>
       </div>
     </div>
   );
