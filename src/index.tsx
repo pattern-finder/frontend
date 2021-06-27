15e650b1616b67fabbdaccb3787842ff25415854
Yoni <firroloni.yoni@gmail.com>
2021-06-27 12:29:09 +0200

    fix npm

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'src/index.css';
-import App from 'src/App';
-import reportWebVitals from 'src/reportWebVitals';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
