4acbee17dde9382a9b41c64351469761c483c605
Yoni <firroloni.yoni@gmail.com>
2021-07-10 22:38:53 +0200

    hardcode API url

--- src/axios-config.ts
+++ src/axios-config.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const Axios = axios.create({
-  // baseURL: 'https://api.picspy.vagahbond.com',
-  baseURL: process.env.REACT_APP_API_EXTERNAL_PORT
-    ? `${process.env.REACT_APP_API_EXTERNAL_HOST}:${process.env.REACT_APP_API_EXTERNAL_PORT}`
-    : process.env.REACT_APP_API_EXTERNAL_HOST,
+  baseURL: 'https://api.picspy.vagahbond.com',
+  // baseURL: process.env.REACT_APP_API_EXTERNAL_PORT
+  //   ? `${process.env.REACT_APP_API_EXTERNAL_HOST}:${process.env.REACT_APP_API_EXTERNAL_PORT}`
+  //   : process.env.REACT_APP_API_EXTERNAL_HOST,
 });
 
 export default Axios;
