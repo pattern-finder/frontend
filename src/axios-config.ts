b1b0a1183418ce12a8f1de9de0f570c4f67b296f
tcatonet <catonethomas@yahoo.fr>
2021-10-04 17:45:19 +0200

    frontend heroku to google cloud

--- src/axios-config.ts
+++ src/axios-config.ts
@@ -4,7 +4,7 @@ const Axios = axios.create({
   baseURL: `${
     process.env.REACT_APP_API_EXTERNAL_HOST
       ? process.env.REACT_APP_API_EXTERNAL_HOST
-      : `https://picspy-api.herokuapp.com/`
+      : `http://34.79.103.41`
   }${
     process.env.REACT_APP_API_EXTERNAL_PORT
       ? `:${process.env.REACT_APP_API_EXTERNAL_PORT}`
