fbf9cc59fc8b085973aa0f9a1f98fb12fbfd372c
IHYATEN imane <imane.ihyaten@gmail.com>
2021-10-17 12:55:20 +0200

    modif

--- frontend/src/components/ChallengeForSeriesComponent.tsx
+++ frontend/src/components/ChallengeForSeriesComponent.tsx
@@ -79,7 +79,7 @@ export const ChallengeListitem = ({
               ) : (
                   <>
                   </>
-
+                )}
               {user._id === getUserSession() ?.sub && (
                 <Link to={`/edit/challenge/${_id}`}>
                   <div className="rounded-lg px-2 py-1 ml-2 bg-black hover:bg-blue-700">
